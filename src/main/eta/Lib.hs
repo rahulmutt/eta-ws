070c6b931b4689c12c2a397cd93b751433c1a16a
Nikita Tchayka <nikitatchayka@gmail.com>
2018-03-26 07:46:34 +0100

    Add eta stuff

---
+++ src/main/eta/Lib.hs
@@ -0,0 +1,6 @@
+module Lib where
+
+@Export
+"@static eta_ws.Lib.foo" foo :: Int -> Int
+foo _ = 3
+
---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import eta_ws.Lib;
+
+public class App {
+    public static void main(String[] args) {
+        //int x = Lib.foo(3);
+        //System.out.println(x);
+    }
+}
